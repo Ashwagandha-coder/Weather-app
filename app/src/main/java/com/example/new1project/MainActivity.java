cc2bb40b87084cd753d1722b3649c6e0bf9c4b60
Ashwagandha-coder <max.cacz@yandex.ru>
2022-02-08 14:36:10 +0500

    add to variables - object

--- app/src/main/java/com/example/new1project/MainActivity.java
+++ app/src/main/java/com/example/new1project/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.new1project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText user_field;
+    private Button main_btn;
+    private TextView result_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user_field = findViewById(R.id.user_field);
+        main_btn = findViewById(R.id.main_btn);
+        result_info = findViewById(R.id.result_info);
     }
 }
