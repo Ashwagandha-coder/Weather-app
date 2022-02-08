54c4e0b9fb0af7e93c0910a74f4d8ad3bf6877f6
Ashwagandha-coder <max.cacz@yandex.ru>
2022-02-08 15:47:31 +0500

    add the Toast

--- app/src/main/java/com/example/new1project/MainActivity.java
+++ app/src/main/java/com/example/new1project/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.new1project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
         user_field = findViewById(R.id.user_field);
         main_btn = findViewById(R.id.main_btn);
         result_info = findViewById(R.id.result_info);
+
+        main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(user_field.getText().toString().trim().equals(""))
+                    Toast.makeText(MainActivity.this,R.string.no_user_input,Toast.LENGTH_LONG).show();
+                else {
+                    
+                }
+            }
+        });
     }
 }
